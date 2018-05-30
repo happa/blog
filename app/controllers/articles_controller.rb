4e340a1f3028310fe7447c14af6dd84872d4489c
yamada <skype.1202.119.t@gmail.com>
2018-05-30 11:30:29 +0000

    Create articles form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,15 @@ class ArticlesController < ApplicationController
     def new
         
     end
+    
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        
+        redirect_to article_path(@article)
+    end
+    
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
 end
